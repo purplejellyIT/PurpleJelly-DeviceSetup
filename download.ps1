19e9bfec38b05bdcd518ed7e742bad4161e7ae20
williamyoung2196 <69459942+williamyoung2196@users.noreply.github.com>
2020-08-10 14:13:19 +0100

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -1,6 +1,6 @@
 Set-ExecutionPolicy remotesigned
 function downloadbuilder {
-Invoke-WebRequest -Uri "https://github.com/williamyoung2196/PurpleJelly-DeviceSetup//archive/master.zip" -OutFile "C:\PurpleJelly\PJDeviceSetup.zip"
+Invoke-WebRequest -Uri "https://github.com/williamyoung2196/PurpleJelly-DeviceSetup/archive/master.zip" -OutFile "C:\PurpleJelly\PJDeviceSetup.zip"
 Expand-Archive C:\PurpleJelly\PJDeviceSetup.zip -DestinationPath C:\PurpleJelly\
 }
 downloadbuilder
