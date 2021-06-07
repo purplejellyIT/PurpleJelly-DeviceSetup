192d1aa6f0cee2c2033294d44a3298b3acdb5d52
williamyoung2196 <69459942+williamyoung2196@users.noreply.github.com>
2021-06-07 14:55:38 +0100

    Update BitlokerConfigurator.bat

--- BitlokerConfigurator.bat
+++ BitlokerConfigurator.bat
@@ -16,4 +16,8 @@ echo Creating the TPM key (used to auto boot encrypted OS)
 powershell.exe -Command "manage-bde.exe -protectors -add %SystemDrive% -tpm >> C:\PurpleJelly\%computername%_bitlockerlog.txt"
 echo Enabling BitLocker Encryption
 powershell.exe -Command "manage-bde.exe -on %systemdrive% >> C:\PurpleJelly\%computername%_bitlockerlog.txt"
+timeout 10
+echo Copying Bitlocker Log to BD-Server "\\bd-server\BitlockerLogs"
+cd C:\PurpleJelly 
+copy "*_bitlockerlog.txt" \\bd-server\BitlockerLogs
 shutdown /r /f /t 300 /c "PC will restart in 300 Seconds, please allow it to Auto restart for Automate to install, Encryption will commence on the reboot.... Please make a copy of the Bitlocker log from C:\PurpleJelly to the USB"
