ee9bb747dea3c17a48efd5e0408336810b45f47b
williamyoung2196 <69459942+williamyoung2196@users.noreply.github.com>
2021-07-06 10:16:40 +0100

    Update RunMe.bat

--- RunMe.bat
+++ RunMe.bat
@@ -1,5 +1,5 @@
 mkdir c:\PurpleJelly
 Powershell.exe -Command "& {Start-Process Powershell.exe -ArgumentList 'set-executionpolicy remotesigned' -Verb RunAs}"
 powershell.exe -ExecutionPolicy Bypass -noprofile -command "&{start-process powershell -ArgumentList '-noprofile -file \"%~dp0download.ps1\"' -verb RunAs}"
-timeout 10
+timeout 30
 powershell.exe -ExecutionPolicy Bypass -noprofile -command "&{start-process powershell -ArgumentList '-noprofile -file \"c:\PurpleJelly\PurpleJelly-DeviceSetup-master\PCSetup.ps1\"' -verb RunAs}"
